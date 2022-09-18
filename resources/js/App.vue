43c8923a27b1efc018d78f596e82b05690dc41e4
ひろろ <hrsi.ad@gmail.com>
2022-09-18 14:34:10 +0900

    画像表示

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,11 @@
 <script setup>
-import Counter from './comonents/Counter.vue';
+import Counter from "./comonents/Counter.vue";
+import logo from "../images/naka.jpg";
 </script>
 
 <template>
     <h1>Hello World</h1>
+    <img :src="logo" />
+    <img src="../images/naka.jpg" />
     <Counter />
 </template>
